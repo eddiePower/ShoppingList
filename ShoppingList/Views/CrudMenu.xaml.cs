02c4d820de1518e11b7e1eb5f6af82135c3d10c6
Eddie Power <eddie.power@icloud.com>
2017-12-23 22:52:56 +1100

    Add project files.

---
+++ ShoppingList/Views/CrudMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ShoppingList.Views
+{
+    /// <summary>
+    /// Interaction logic for CrudMenu.xaml
+    /// </summary>
+    public partial class CrudMenu : UserControl
+    {
+        public CrudMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ShoppingList/Views/ShoppingList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ShoppingList.Views
+{
+    /// <summary>
+    /// Interaction logic for ShoppingList.xaml
+    /// </summary>
+    public partial class ShoppingList : UserControl
+    {
+        public ShoppingList()
+        {
+            InitializeComponent();
+        }
+    }
+}
